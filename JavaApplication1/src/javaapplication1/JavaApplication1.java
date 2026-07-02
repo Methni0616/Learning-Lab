69ab2d1a00aa3c61e7aa69a0fe3c807811114475
Methni0616 <methnimanarandi7@gmail.com>
2026-07-02 11:48:02 +0530

    Java Lesson 05: Learn Scanner user input

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,19 +1,27 @@
 package javaapplication1;
 
+import java.util.Scanner;
+
 public class JavaApplication1 {
 
     public static void main(String[] args) {
 
-        String name = "Methni";
-        int age = 23;
-        double gpa = 3.48;
-        boolean isIntern = true;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.print("Are you an intern? (true/false): ");
+        boolean intern = input.nextBoolean();
+
+        System.out.println();
+        System.out.println("----- Student Details -----");
         System.out.println("Name : " + name);
-        System.out.println("Age : " + age);
-        System.out.println("GPA : " + gpa);
-        System.out.println("Intern : " + isIntern);
-
+        System.out.println("Age  : " + age);
+        System.out.println("Intern : " + intern);
+        input.close();
     }
-
 }
